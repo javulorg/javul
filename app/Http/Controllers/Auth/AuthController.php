79291d319da7b99c85cc952fc904b3d4114004ed
Aadhya Tech <anant.panchal@live.com>
2016-05-27 18:08:26 +0530

    remove index method from authenticate middleware

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -37,6 +37,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
+        view()->share('user_login',\Auth::check());
         $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
     }
 
