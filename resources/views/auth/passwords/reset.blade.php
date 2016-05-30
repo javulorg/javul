cafd7b3a96915ca110de6dec71fe9c051262cb5c
Aadhya Tech <anant.panchal@live.com>
2016-05-30 18:28:31 +0530

    worked on forgot password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -52,7 +52,7 @@
 
                 <div class="row form-group">
                     <div class="col-sm-12">
-                        <button type="submit" class="btn btn-primary">
+                        <button type="submit" class="btn orange-bg">
                             <i class="fa fa-btn fa-refresh"></i>&nbsp;{!! Lang::get('messages.reset_password') !!}
                         </button>
                     </div>
