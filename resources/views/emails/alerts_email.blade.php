a042ffe12e7648dd58c6cb6004ebf47cedb3cff2
Aadhya Tech <anant.panchal@live.com>
2017-03-31 18:45:03 +0530

    worked on issues (uploaded)

---
+++ resources/views/emails/alerts_email.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.email')
+@section('content')
+    <h3>Hi {{$userObj->first_name.' '.$userObj->last_name}},</h3>
+    <p>{!! $content !!}</p>
+    <br/>
+    <p style="display: inline-block;text-align: center;width:100%;font-size: 13px;">
+        <a href="{!! url('account/manage_alerts') !!}" style="display:inline-block">Manage Alert Setting</a>&nbsp;|&nbsp;
+        <a href="{!! url('my_watchlist')!!}" style="display:inline-block">Edit Watchlist</a>
+    </p>
+    <p>Regards,</p>
+    <p>info@javul.org</p>
+@endsection
