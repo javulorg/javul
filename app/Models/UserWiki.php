1b8278452a775c48cce6b2cfe48b5400089c32b1
Mahmoud Omar <m.ahmedhussienomar@gmail.com>
2023-07-12 18:05:25 +0200

    delete

--- app/Models/UserWiki.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class UserWiki extends Model
-{
-    protected $table = 'userwiki_page';
-    protected $fillable = ['page_content','page_title','user_id'];
-}
--- app/UserWiki.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App;
-use Illuminate\Database\Eloquent\Model;
-class UserWiki extends Model
-{
-    protected $table = 'userwiki_page';
-    protected $fillable = ['page_content','page_title','user_id'];
-}
