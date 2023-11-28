a1752f51f5694ba96f343ba3d1aa861f5ec29618
Mahmoud Omar <m.ahmedhussienomar@gmail.com>
2023-11-28 14:26:27 +0200

    Delete old website

--- resources/views/emails/registration.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.email')
-@section('content')
-<h3>Hi, {{$userObj->first_name.' '.$userObj->last_name}}</h3>
-<p>Welcome to Javul.org</p><br/>
-<p>Regards,</p>
-<p>info@javul.org</p>
-@endsection
